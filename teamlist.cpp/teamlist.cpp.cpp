879805be91dd92580b7be4c90e444c58d650604d
Kamron200 <96528746+komron2000@users.noreply.github.com>
2024-10-31 08:17:33 +0500

    Add files via upload

---
+++ teamlist.cpp/teamlist.cpp.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+
+const int LEN = 80;
+
+class employee
+{
+private:
+	char name[LEN];
+	unsigned long number;
+public:
+	friend istream& operator>>(istream& s, employee& e);
+	friend ostream& operator>>(ostream& s, employee& e);
+};
